03d5b98fcab48f1196ebf5e2204691fe012e79da
DarylNg514 <ngueufangdaryl99@gmail.com>
2024-10-20 05:07:13 -0400

    update setting
    update setting

--- Projet_stage/Projet_stage/settings.py
+++ Projet_stage/Projet_stage/settings.py
@@ -163,6 +163,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# This is where Django will collect static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
